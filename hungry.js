4d12524b269e607c6cdcc1783f1a0bb8c4b6e509
Monaim <monaimmukul75@gmail.com>
2021-02-09 12:40:20 +0600

    continue

--- hungry.js
+++ hungry.js
@@ -1,11 +1,52 @@
-fetch(' https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res=> res.json())
-.then(data=>displayItems(data));
+const searchmeals = () => {
+    const searchText = document.getElementById('search-field').value;
+    console.log(searchText);
 
- const displayItems = categories =>{
-     for (let i = 0; i < categories.length; i++) {
-         const element = categories[i];
-         
-         console.log(element.strCategory);
-     }
- }
+};
+
+
+const GetMeal =() =>{
+    const search= document.getElementById("SearchMeal").value;
+    const url=`https://www.themealdb.com/api/json/v1/1/categories.php/${search}`
+    fetch(url)
+    .then(res => res.json())
+    .then (data => displayMeals(data.categories));
+}
+
+const displayMeals = meals =>{
+    const newMeal=document.getElementById("list");
+    newMeal.innerHTML= ' ';
+   meals.forEach(meal => {
+      const mealDiv =document.createElement("div");
+      mealDiv.className="row"
+      mealDiv.innerHTML=
+      `<div class="col-lg-4">
+      <div class="mealitems">
+        <img src="${meal.strCategoryThumb}" onclick="ImageClick()" alt="">
+        <h5>${meal.strCategory}</h5>
+      </div>
+    </div>
+
+    <div class="col-lg-4">
+    <div class="mealitems">
+      <img src="${meal.strCategoryThumb}" onclick="ImageClick()" alt="">
+      <h5>${meal.strCategory}</h5>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="mealitems">
+      <img src="${meal.strCategoryThumb}" onclick="ImageClick()" alt="">
+      <h5>${meal.strCategory}</h5>
+    </div>
+  </div>
+      `
+      newMeal.appendChild(mealDiv);
+   })
+       
+}
+
+const ImageClick = () =>{
+    window.alert() ("sometext","defaultText");
+}
+
